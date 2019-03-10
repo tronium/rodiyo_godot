cd2bb7e20770c076befe82918d228a44810705b6
ori <cargmax@gmail.com>
2019-03-10 22:33:14 +0100

    emulate left mouse press and release on the usage example

--- udp_server/scripts/udp_server_usage.gd
+++ udp_server/scripts/udp_server_usage.gd
@@ -7,7 +7,7 @@
 extends Node
 
 func _ready():
-	var th = load("res://udp_server.gd")
+	var th = load("res://scripts/udp_server.gd")
 	var svr = th.new("0.0.0.0", 8099)
 	svr.connect("incoming", self, "consume")
 
@@ -23,3 +23,22 @@ func consume(s, m):
 
 	print("count: ", i, " - data: ", m.get_string_from_ascii())
 	i = i + 1
+
+	#return
+
+	if m.get_string_from_ascii() == "0":
+		call_deferred("emulate_left_mouse_press")
+	else:
+		call_deferred("emulate_left_mouse_release")
+
+func emulate_left_mouse_press():
+	var mb = InputEventMouseButton.new()
+	mb.set_button_index(1)
+	mb.set_pressed(true)
+	Input.parse_input_event(mb)
+
+func emulate_left_mouse_release():
+	var mb = InputEventMouseButton.new()
+	mb.set_button_index(1)
+	mb.set_pressed(false)
+	Input.parse_input_event(mb)
